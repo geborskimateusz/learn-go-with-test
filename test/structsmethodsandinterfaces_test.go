8796cbed7f80b0c5054c817c8b3add9551855123
geborskimateusz <mateuszgeborski@gmail.com>
2020-09-15 15:19:51 +0200

    added test tables to geometry

--- test/structsmethodsandinterfaces_test.go
+++ test/structsmethodsandinterfaces_test.go
@@ -7,36 +7,51 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	rectangle := geometry.Rectangle{10, 20}
+	rectangle := geometry.Rectangle{Width: 10, Height: 20}
 	got := geometry.Perimeter(rectangle)
 	want := 60.00
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	assertEqual(t, got, want)
 }
 
 func TestArea(t *testing.T) {
-	assertEqual := func(t *testing.T, got, want float64) {
-		t.Helper()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	}
-
-	calculateArea := func(t *testing.T, s geometry.Shape, want float64) {
-		assertEqual(t, s.Area(), want)
-	}
 
 	t.Run("Calculate area of circle", func(t *testing.T) {
-		circle := geometry.Circle{20}
+		circle := geometry.Circle{Radius: 20}
 		want := 1256.00
 		calculateArea(t, circle, want)
 	})
 
 	t.Run("Calculate area of rectangle", func(t *testing.T) {
-		rectangle := geometry.Rectangle{20.00, 20.00}
+		rectangle := geometry.Rectangle{Width: 20.00, Height: 20.00}
 		want := 400.00
 		calculateArea(t, rectangle, want)
 	})
+}
+
+func TestAreaTableDriven(t *testing.T) {
+	arrTests := []TestTableStruct{
+		{geometry.Circle{Radius: 20}, 1256.00},
+		{geometry.Triangle{Base: 2, Height: 5}, 5},
+		{geometry.Rectangle{Width: 20.00, Height: 20.00}, 400.00},
+	}
+
+	for _, tt := range arrTests {
+		calculateArea(t, tt.shape, tt.want)
+	}
+}
+
+func assertEqual(t *testing.T, got, want float64) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
+}
+
+func calculateArea(t *testing.T, s geometry.Shape, want float64) {
+	assertEqual(t, s.Area(), want)
+}
+
+type TestTableStruct struct {
+	shape geometry.Shape
+	want  float64
 }
