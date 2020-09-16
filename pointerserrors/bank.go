c3af3675cc4b39f64bf22de539fff74f5e1375fb
geborskimateusz <mateuszgeborski@gmail.com>
2020-09-16 15:08:45 +0200

    pointerserrors

--- pointerserrors/bank.go
+++ pointerserrors/bank.go
@@ -1,13 +1,32 @@
 package pointerserrors
 
+import (
+	"fmt"
+)
+
+// Bitcoin represents number of bitcoins
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+// Wallet stores number of bitcoins
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(ammount int) {
-	w.balance += ammount
+// Deposit amount of bitcoins
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+// Withdraw amount of bitcoins
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+// Balance returns current amount of bitcoins
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
