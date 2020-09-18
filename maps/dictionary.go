29566e8877ed75851fe06bed9c601fd051bdc65e
geborskimateusz <mateuszgeborski@gmail.com>
2020-09-18 12:35:35 +0200

    working with maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,34 @@
 package maps
 
+import (
+	"errors"
+)
+
+// ErrKeyNotFound thrown when key not found in map
+var ErrKeyNotFound = errors.New("There is no such key in dictionary")
+var ErrDuplicateKey = errors.New("Key already exist")
+
+// Dictionary stores key:value map of strings
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+// Search for key in Dictoinary
+func (d Dictionary) Search(key string) (string, error) {
+	foundEl, ok := d[key]
+
+	if !ok {
+		return "", ErrKeyNotFound
+	}
+	return foundEl, nil
+}
+
+// Add unique values to dictrionary
+func (d Dictionary) Add(key, value string) error {
+	_, ok := d[key]
+
+	if ok {
+		return ErrDuplicateKey
+	}
+
+	d[key] = value
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,17 +3,67 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("on known word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+		given := "test"
+		got, _ := dictionary.Search(given)
+		want := "this is just a test"
 
-	assertStrings(t, got, want)
+		assertStrings(t, got, want)
+
+	})
+
+	t.Run("throws ErrKeyNotFound", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+
+		given := "unknown"
+		_, err := dictionary.Search(given)
+		want := ErrKeyNotFound
+
+		asssertError(t, err, want)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("add to a map", func(t *testing.T) {
+		givenKey := "test"
+		givenVal := "this is just a test"
+
+		dictionary := Dictionary{}
+
+		dictionary.Add(givenKey, givenVal)
+
+		got, _ := dictionary.Search(givenKey)
+		assertStrings(t, got, givenVal)
+	})
+
+	t.Run("throws ErrDuplicateKey", func(t *testing.T) {
+		givenKey := "test"
+		givenVal := "this is just a test"
+
+		dictionary := Dictionary{}
+
+		dictionary.Add(givenKey, givenVal)
+
+		err := dictionary.Add(givenKey, givenVal)
+
+		asssertError(t, err, ErrDuplicateKey)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func asssertError(t *testing.T, err, want error) {
+	t.Helper()
+	if err == nil {
+		t.Fatal("Expected error got nil")
+	}
+
+	assertStrings(t, ErrKeyNotFound.Error(), want.Error())
 }
